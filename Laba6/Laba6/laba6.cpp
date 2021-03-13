e78ddae16d91b31261f8ec141ac82079787d6b5b
Smisiw <kirill.kazancev.2001@mail.ru>
2021-03-13 20:51:28 +0700

    kk

--- Laba6/Laba6/laba6.cpp
+++ Laba6/Laba6/laba6.cpp
@@ -8,30 +8,65 @@ struct btree {
     struct btree* right;
 };
 
-void tree_print(btree *tr1) {
+void tree_print(btree* tr1) {
     if (tr1 != NULL) {
-        for (int i = 0; i < tr1->n; i++) {
-            cout << tr1->A[i];
-        }
         tree_print(tr1->left);
+        for (int i = 0; i < tr1->n; i++) {
+            cout << tr1->A[i] << " ";
+        }
         tree_print(tr1->right);
     }
 }
 
-struct btree* add(int b, btree* tree) {
-    if (tree == NULL) {
+void add(int b, btree*& tree) {
+    if (tree == NULL) { //если нет узла
         tree = new btree();
-        for (int i = 0; i < 10; i++) {
-            
-            tree->A[i] = b[i];
+        tree->A[0] = b;
+        tree->n++;
+    }
+    else if (tree->n < 4) { //если узел не заполнен
+        tree->A[tree->n] = b;
+        tree->n++;
+        for (int i = 0; i < tree->n - 1; i++) //сортировка
+            for (int j = 0; j < tree->n - i - 1; j++)
+                if (tree->A[j] > tree->A[j + 1]) {
+                    int c = tree->A[j]; tree->A[j] = tree->A[j + 1]; tree->A[j + 1] = c;
+                }
+    }
+    else if (tree->n == 4) { //если узел заполнен
+        if (b >= tree->A[3]) add(b, tree->right); //если больше правого элемента узла
+        else if (b <= tree->A[0]) add(b, tree->left); //если меньше левого элемента узла
+        else if (b > tree->A[1]) { //если правее центра узла
+            int c = tree->A[3]; tree->A[3] = b; b = c; //замена
+            for (int i = 0; i < tree->n - 1; i++) //сортировка
+                for (int j = 0; j < tree->n - i - 1; j++)
+                    if (tree->A[j] > tree->A[j + 1]) {
+                        int c = tree->A[j]; tree->A[j] = tree->A[j + 1]; tree->A[j + 1] = c;
+                    }
+            add(b, tree->right);
+        }
+        else if (b <= tree->A[1]) { //если левее центра узла
+            int c = tree->A[0]; tree->A[0] = b; b = c; //замена
+            for (int i = 0; i < tree->n - 1; i++) //сортировка
+                for (int j = 0; j < tree->n - i - 1; j++)
+                    if (tree->A[j] > tree->A[j + 1]) {
+                        int c = tree->A[j]; tree->A[j] = tree->A[j + 1]; tree->A[j + 1] = c;
+                    }
+            add(b, tree->left);
         }
     }
-    else if (b < tree->A) 
 }
 
 int main()
 {
-
+    btree* tree = NULL;
+    int n, x;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        add(x, tree);
+    }
+    tree_print(tree);
     return 0;
 }
 
