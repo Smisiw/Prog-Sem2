9297b011915e56609a6940d5b6ce8c6691821e8c
Smisiw <kirill.kazancev.2001@mail.ru>
2021-03-13 17:48:12 +0700

    Kok

--- Laba5/Laba5/Laba5.cpp
+++ Laba5/Laba5/Laba5.cpp
@@ -1,8 +1,50 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+long long N = LONG_MAX;
+
+//сумма цифр числа
+long long sum_num(long long x) {
+	long long sum = 0;
+	for (; x > 0; x /= 10) {
+		sum += x % 10;
+	}
+	return sum;
+}
+
+long long recursia(long long a) {
+	long long v = a;
+	if (a < 100000000000000000) {
+		a *= 10;
+		a += 3;
+		if (a % 21 == 0 && sum_num(a) % 21 == 0) {
+			return a;
+		}
+		v = recursia(a);
+		if (v != -1) a = v;
+		if (a % 21 == 0 && sum_num(a) % 21 == 0) {
+			return a;
+		}
+		a /= 10;
+		a *= 10;
+		a += 7;
+		if (a % 21 == 0 && sum_num(a) % 21 == 0) {
+			return a;
+		}
+		v = recursia(a);
+		if (v != -1) a = v;
+		if (a % 21 == 0 && sum_num(a) % 21 == 0) {
+			return a;
+		}
+		return -1;
+	}
+	else {
+		return -1;
+	}
+}
+
+
+int main() {
+	cout << recursia(0);
+	return 0;
 }
